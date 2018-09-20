a306d38d21e27b5930ad47d5ee5f4db029e90f34
Mark Farrell <mark.farrell@csiro.au>
2018-09-20 12:46:13 +1000

    dirs set up, data in correct place

---
+++ scripts/dir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#***************************************************************#
+#              makes directory structure                        #
+#***************************************************************#
+
+# should be run from the root of your working directory
+
+mkdir data/
+mkdir data/raw/
+mkdir data/working/
+mkdir data/processed/
+mkdir scripts/
+mkdir outputs/
+mkdir logs/
+mv dir.sh scripts/
